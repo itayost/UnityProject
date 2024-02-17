33bb50b586876d4e2e70bf859d443db6a93c370d
itayost <107315278+itayost@users.noreply.github.com>
2024-02-17 18:25:30 +0200

    added door and gate movement

---
+++ Assets/Scripts/CarGateMotion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGateMotion : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); //connect to Unity component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("IsOpen", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
