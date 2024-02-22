2a666139dda2270cabfa8ca725b1467e1e0aed52
itayost <107315278+itayost@users.noreply.github.com>
2024-02-22 20:58:29 +0200

    added moving and walking NPC's

--- Assets/Scripts/AJBehaviour.cs
+++ Assets/Scripts/AJBehaviour.cs
@@ -1,26 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AJBehaviour : MonoBehaviour
 {
     Animator animator;
+    NavMeshAgent agent;
+    public GameObject target;
+    public GameObject point1;
+    public GameObject point2;
+    bool goes_to_pt1 = true;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
         if(Input.GetKeyDown(KeyCode.Q))
         {
             animator.SetInteger("State", 1);
+            //compute the path to the target and starts moving NPC
+            agent.SetDestination(target.transform.position);
+            if(agent.isStopped)
+            {
+                agent.isStopped = false;
+            }
         }
-        if(Input.GetKeyDown(KeyCode.X))
+        if(Input.GetKeyDown(KeyCode.X) || distance < 2)
         {
             animator.SetInteger("State", 0);
+            agent.isStopped = true;
+        }
+        if(distance < 3)
+        {
+            if(goes_to_pt1 == true)
+            {
+                target.transform.position = point2.transform.position;
+                agent.SetDestination(target.transform.position);
+                goes_to_pt1 = false;
+            }
+            else
+            {
+                target.transform.position = point1.transform.position;
+                agent.SetDestination(target.transform.position);
+                goes_to_pt1 = true;
+            }
         }
     }
 }
